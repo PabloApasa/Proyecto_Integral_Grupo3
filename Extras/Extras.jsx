3ba7744c4d291524d1c4f14a1c9c665fce803f58
xxx2131N <165327222+xxx2131N@users.noreply.github.com>
2025-10-26 21:47:18 -0300

    Update Extras.jsx

--- Extras/Extras.jsx
+++ Extras/Extras.jsx
@@ -1,46 +1,47 @@
 import { useState } from "react";
+
 function App() {
- 
-    const [page, setPage] = useState("Home");
-    const [loagged, setLoagged] = useState(false);
+  const [page, setPage] = useState("Home");
+  const [logged, setLogged] = useState(false);
 
-    const go = (page) => setPage(page);
+  const go = (page) => setPage(page);
 
-    return {
-        <div style= { textAlign: "center", marginTop: "20px" }>
-            <h1> proyecto integrel Gupo 3</h1>
+  return (
+    <div style={{ textAlign: "center", marginTop: "20px" }}>
+      <h1>Proyecto Integral - Grupo 3</h1>
 
-            <nav>
-                <button onClick={() => go("Home")}>Home</button>
-                <button onClick={() => go("About")}>About</button>
-                <button onClick={() => go("Perfil")}>Perfil</button>
-            </nav>
+      <nav>
+        <button onClick={() => go("Home")}>Home</button>
+        <button onClick={() => go("About")}>About</button>
+        <button onClick={() => go("Perfil")}>Perfil</button>
+      </nav>
 
-            <hr />
+      <hr />
 
-            {page === "Home" && <h2>Bienvenido a la pagina principal</h2>}
+      {page === "Home" && <h2>Bienvenido a la página principal</h2>}
 
-            {page === "About" && <h2>Esta es la pagina de informacion</h2>}
+      {page === "About" && <h2>Esta es la página de información</h2>}
 
-            {page === "Perfil" && (
-                loagged ? (
-                    <div>
-                        <h2>perfil del usaurio</h2>
-                        <p>Nombre: Juan Perez</p>
-                        <p>Email: calbomagenta69@gmail.com</p>
-                        <button onClick={() => setLoagged(false)}>Cerrar Sesion</button>
-                    </div>
-                ) : (
-                    <div>
-                        <h2>Por favor inicia sesion</h2>
-                        <button onClick={() => setLoagged(true)}>Iniciar Sesion</button>
-                    </div>
-                )
-            )}
-            {page !== "Home" && page !== "About" && page !== "Perfil" && ( 
-            <h2>no sea encontrado la pagina solicitada</h2>  )}
-        </div>
-    );
-    }
+      {page === "Perfil" &&
+        (logged ? (
+          <div>
+            <h2>Perfil del usuario</h2>
+            <p>Nombre: Juan Pérez</p>
+            <p>Email: calbomagenta69@gmail.com</p>
+            <button onClick={() => setLogged(false)}>Cerrar sesión</button>
+          </div>
+        ) : (
+          <div>
+            <h2>Por favor, inicia sesión</h2>
+            <button onClick={() => setLogged(true)}>Iniciar sesión</button>
+          </div>
+        ))}
+
+      {page !== "Home" && page !== "About" && page !== "Perfil" && (
+        <h2>la paguina no ha sido encontrada</h2>
+      )}
+    </div>
+  );
 }
- export default App;
+
+export default App;
