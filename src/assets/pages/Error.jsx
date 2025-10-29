7448df02828e83e3a9392d1cb4085adb0f85a5f3
xxx2131N <165327222+xxx2131N@users.noreply.github.com>
2025-10-29 04:05:47 -0300

    areglo del jeugo estrella, importacion del error y un css
    los tenia ya echos pero por temas familiares no los subi aun pero seguian en la pc

--- src/assets/pages/Error.jsx
+++ src/assets/pages/Error.jsx
@@ -1,6 +1,12 @@
+import "./Error.css";
 function Error() {
     return (
-        <h1>Error</h1>
+        <div className="error-page">
+            <h1 className="error ">Error</h1>
+            <h2>la pagina no sea h encontraod</h2>
+            <p>no haceptamos lloros pero la pagina no existe</p>
+            <link to="/">volver a la pagina principal</link>
+        </div>
     )
 };
 
