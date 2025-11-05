8a0c85bcc939b9a3e8607d3c270493cfd3284002
AldiRQ <negronitica@gmail.com>
2025-11-05 11:34:09 -0300

    Aldana Quiroga: Modificacion del Formulario
    Se modifico el diseño del Formulario Registro y cambio los nombres de los juegos de Diagnostico.

---
+++ src/assets/components/Diagnostico/Juego02/Imagenes.jsx
@@ -0,0 +1,9 @@
+function Imagenes() {
+    return (
+        <div style={{ textAlign: "center", padding: "50px" }}>
+            <h1>Juego de Imagenes 🎮</h1>
+        </div>
+    );
+}
+
+export default Imagenes;
---
+++ src/assets/components/Diagnostico/Juego04/Juego04.jsx
@@ -0,0 +1,10 @@
+function Juego04() {
+    return (
+        <div style={{ textAlign: "center", padding: "50px" }}>
+            <h1>Aca vs el ultimo juego 🎮</h1>
+            
+        </div>
+    );
+}
+
+export default Juego04;
